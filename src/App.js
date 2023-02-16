5c37ee00f63ae09a73e9d160e0735bc2204cb92c
sameeramin2015 <sameeramin2015@gmail.com>
2023-02-16 14:44:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>welcome to coding challange web application</h1>
+    </div>
+  );
+}
+
+export default App;
